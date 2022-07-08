b5cb9d665794d4c197f685738b96bbb12d317897
rsalesfreitas <rsales.freitas@live.com>
2022-07-08 13:45:56 -0300

    retirar do app a chamada do backend para criação da ec2

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket = "s3-tfstates-desenvolvimento"
-    key    = "aulaaovivo29082020/terraform-dev.tfstate"
-    region = "us-west-2"
-  }
-}
