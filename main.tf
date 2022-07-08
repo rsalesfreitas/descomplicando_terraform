2bf90a3c4450f241b8d73bd74159b0b515dc6d75
rsalesfreitas <rsales.freitas@live.com>
2022-07-08 12:30:47 -0300

    app-infra utilizando chamada de modulo ec2

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = var.aws_region
+}
+
+module "ec2" {
+  source = "git@github.com:rsalesfreitas/descomplicando_terraform_module_ec2.git?ref=main"
+  app_name = "turma1"
+  aws_instance = "t3.micro"
+}
+
+output "ip_address_ec2" {
+  value = module.ec2.ip_address
+}
